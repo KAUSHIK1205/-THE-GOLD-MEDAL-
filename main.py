6d571833f131614e9f004a059d5ed9d1dec65d87
KAUSHIK1205 <chezhiworld@gmail.com>
2024-09-17 18:51:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print ("conrats\n on gettin your gold medal in your state games\n")
+print("do you know to congrats in reverse ?\n")
+print("stargnoc\n")
+print("that was fun right?:)\n")
+print("I HAVE TO TURN IT INTO UPPERCASE \n")
+print("mmm.....I HAVE AN IDEA")
+
+
+original_text = "conrats\n on gettin your gold medal in your state games"
+originaltext="do you know to congrats in reverse ?"
+orginalstatement="stargnoc\nthat was fun right?:)"
+upper_text = original_text.upper()
+uppertext = originaltext.upper()
+upperotext = orginalstatement.upper()
+
+print (upper_text)
+
+print(uppertext)
+print(upperotext)
